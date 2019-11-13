e7aa8148543f7635c6eac51224a988b9bd6c6b47
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-13 17:39:13 +0700

    frontend: use ReactJS as front-end

---
+++ frontend/src/components/EmptyState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Item from "./Item";
+
+export default function EmptyState(props){
+
+    return(
+        <div>
+            <center>
+                <h5>Belum ada item yang dipilih</h5>
+                <p>Klik salah satu item di daftar Menu</p>
+            </center>
+        </div>
+    )
+}
